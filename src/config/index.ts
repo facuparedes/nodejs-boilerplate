59c6a303174de4298b42e7c1d934a778c4b2435c
Facundo Paredes <fparedes45@gmail.com>
2021-03-25 00:43:23 -0300

    Adding dotenv to project

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  NODE_ENV: process.env.NODE_ENV || "development",
+  HOST: process.env.HOST || "127.0.0.1",
+  PORT: process.env.PORT || 3000,
+};
