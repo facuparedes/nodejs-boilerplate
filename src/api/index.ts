698b7542f56a97d474b12cc5256bf5ee2eaa79fe
Facundo Paredes <fparedes45@gmail.com>
2021-04-30 21:03:15 -0300

    Adding routes support

---
+++ src/api/index.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+// import api from "./routes/v1";
+const router = Router();
+
+// router.use("/v1", api);
+
+export default router;
--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,5 +1,7 @@
 import express, { Express } from "express";
+import routes from "../api";
 
 export default async function (app: Express) {
   app.use(express.json());
+  app.use("/", routes);
 }
